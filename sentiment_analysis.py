d7e6cd35723719afbe75b1d89a2424668c35a20b
alejandracast2 <marialejac120@gmail.com>
2026-05-12 21:01:58 -0500

    tarea 3

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,13 +1,25 @@
 import requests 
-# Importar la biblioteca requests para manejar solicitudes HTTP
+import json
 
 def sentiment_analyzer(text_to_analyse): 
-    # Definir una función llamada sentiment_analyzer que toma una entrada de tipo cadena (text_to_analyse)
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict' 
     # URL del servicio de análisis de sentimientos 
-    myobj = { "raw_document": { "text": text_to_analyse } } 
-    # Crear un diccionario con el texto a analizar 
-    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"} 
-    # Establecer las cabeceras requeridas para la solicitud API 
-    response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+
+    # Construyendo la carga útil de la solicitud en el formato esperado 
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Encabezado personalizado especificando el ID del modelo para el servicio de análisis de sentimientos 
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+
+    # Enviando una solicitud POST a la API de análisis de sentimientos 
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Analizando la respuesta JSON de la API 
+    formatted_response = json.loads(response.text)
+
+    # Extrayendo la etiqueta de sentimiento y la puntuación de la respuesta 
+    label = formatted_response['documentSentiment']['label'] 
+    score = formatted_response['documentSentiment']['score']
+
+    # Devolviendo un diccionario que contiene los resultados del análisis de sentimientos 
+    return {'label': label, 'score': score}
