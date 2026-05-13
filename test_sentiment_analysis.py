3421cb451f65823cda66fde1dd6eb6c7a0d4b006
alejandracast2 <marialejac120@gmail.com>
2026-05-12 21:29:57 -0500

    correccion tarea 5

--- test_sentiment_analysis.py
+++ test_sentiment_analysis.py
@@ -4,10 +4,13 @@ import unittest
 class TestSentimentAnalyzer(unittest.TestCase): 
     def test_sentiment_analyzer(self):
         # Caso de prueba para sentimiento positivo 
-        result_1 = sentiment_analyzer('I love working with Python') self.assertEqual(result_1['label'], 'SENT_POSITIVE') 
+        result_1 = sentiment_analyzer('I love working with Python') 
+        self.assertEqual(result_1['label'], 'SENT_POSITIVE') 
         # Caso de prueba para sentimiento negativo 
-        result_2 = sentiment_analyzer('I hate working with Python') self.assertEqual(result_2['label'], 'SENT_NEGATIVE') 
+        result_2 = sentiment_analyzer('I hate working with Python') 
+        self.assertEqual(result_2['label'], 'SENT_NEGATIVE') 
         # Caso de prueba para sentimiento neutral 
-        result_3 = sentiment_analyzer('I am neutral on Python') self.assertEqual(result_3['label'], 'SENT_NEUTRAL')
+        result_3 = sentiment_analyzer('I am neutral on Python') 
+        self.assertEqual(result_3['label'], 'SENT_NEUTRAL')
 
 unittest.main()
